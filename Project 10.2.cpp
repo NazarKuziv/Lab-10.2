910c155cbf4bbcbef00152e486bc665b825bd44c
Nazar Kuziv <nazar123kuziv@gmail.com>
2021-04-06 19:24:54 +0300

    first commit

---
+++ Project 10.2.cpp
@@ -0,0 +1,32 @@
+﻿// Lab_10_2.cpp
+// < Кузів Назар >
+// Лабораторна робота № 10.2
+// Пошук слів у текстовому файлі
+// Варіант 6
+
+
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+int CountWords(ifstream& f, int kilk)
+{
+	char words[100];
+	f.open("t.txt");
+	while (f >> words)
+	{
+		kilk++;
+	}
+	f.close();
+	return kilk;
+}
+
+int main()
+{
+	ifstream f;
+	cout << "The number of words in the file: "<< CountWords(f, 0) <<endl;
+
+	return 0;
+}
